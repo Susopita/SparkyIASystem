88e7b8bcbf1882457191178baafb49d49e46865f
Leonardo <leocrack107chrios@gmail.com>
2025-05-07 16:41:55 -0500

    First Commit : Base

---
+++ sparky/src/test/java/io/reflectoring/sparky/SparkyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.reflectoring.sparky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
