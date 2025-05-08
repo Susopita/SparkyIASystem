3daf539939ad822c38ecf8090161c4c0d6c25506
Susopita <thesusot@gmail.com>
2025-05-07 19:42:22 -0500

    feat(UsuarioFinal): Creacion parcial de Usuario Final

--- src/main/java/io/reflectoring/sparky/usuarioFinal/domain/UsuarioFinal.java
+++ src/main/java/io/reflectoring/sparky/usuarioFinal/domain/UsuarioFinal.java
@@ -1,5 +1,30 @@
 package io.reflectoring.sparky.usuarioFinal.domain;
 
-public class UsuarioFinal {
+import java.util.List;
 
+import io.reflectoring.sparky.solicitud.domain.Solicitud;
+import io.reflectoring.sparky.usuario.domain.Usuario;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "usuarioFinal")
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Inheritance(strategy = InheritanceType.JOINED)
+@PrimaryKeyJoinColumn(name = "id")
+public class UsuarioFinal extends Usuario {
+
+    @OneToMany(mappedBy = "usuarioFinal")
+    private List<Solicitud> solicitudes;
 }
